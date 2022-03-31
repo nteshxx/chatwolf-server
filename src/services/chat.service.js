18f32c88a4be28b8650a3f74e38b855f1cdb56c5
Nitesh Yadav <nteshxx@gmail.com>
2022-03-31 10:39:51 +0530

    endpoint added: get user's conversations with last message & timestamp

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
 const { Chat, Message, User } = require('../models');
 
 const pushMessage = async (senderId, receiverId, chatId, text, attachment) => {
@@ -20,13 +22,44 @@ const createNewChat = async (userId, chatId) => {
 };
 
 const retrieveAllChats = async (userId) => {
-  const allChatsId = await Message.find({ $or: [{ senderId: userId }, { receiverId: userId }] }).distinct('chatId');
-  const participants = allChatsId
-    .reduce((prev, curr) => `${prev}-${curr}`)
-    .split('-')
-    .filter((id) => id !== userId);
-  const users = await User.find({ _id: { $in: participants } }, '_id name');
-  return users;
+  // get all messages of a user and group by unique receivers in descending order
+  const allChats = await Message.aggregate([
+    { $match: { $or: [{ senderId: mongoose.Types.ObjectId(userId) }, { receiverId: mongoose.Types.ObjectId(userId) }] } },
+    { $project: { chatId: 1, receiverId: 1, senderId: 1, text: 1, attachment: 1, createdAt: 1 } },
+    { $sort: { createdAt: -1 } },
+    {
+      $group: {
+        _id: '$chatId',
+        senderId: { $first: '$senderId' },
+        receiverId: { $first: '$receiverId' },
+        text: { $first: '$text' },
+        attachment: { $first: '$attachment' },
+        timeStamp: { $first: '$createdAt' },
+        numberOfMessages: { $sum: 1 },
+      },
+    },
+  ]);
+
+  // get name of all unique receivers
+  const participants = allChats.reduce((prev, curr) => ({ _id: `${prev._id}-${curr._id}` }));
+  const ids = participants._id.split('-').filter((id) => id !== userId);
+  const users = await User.find({ _id: { $in: ids } }, '_id name');
+  const modUsers = users.map((user) => ({
+    name: user.name,
+    userid: user._id.toString(),
+  }));
+
+  // merge receivers name and thier last message
+  const result = [];
+  for (let i = 0; i < allChats.length; i += 1) {
+    result.push({
+      ...allChats[i],
+      ...modUsers.find(
+        (user) => user.userid === allChats[i].senderId.toString() || user.userid === allChats[i].receiverId.toString()
+      ),
+    });
+  }
+  return result;
 };
 
 module.exports = {
